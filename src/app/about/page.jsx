6b38290117a8cacd6b7d71add6b454a8f9ce5a3c
Amanshah53 <amangup53@gmail.com>
2024-07-20 13:30:36 +0545

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to or movie database website! We are a team of passionate movie
+        enethusiasts who have come together to create a one-stop destination for
+        all you movie-related needs.
+      </p>
+      <p>
+        Our website is designed to provide you with a comprehensive database of
+        moviese from all arond the world, along with the latest news, reviews,
+        and trailers. Our movie database is constantly updated with newe
+        release, ensuring that you have access to the latest and greatest in the
+        world of cinema. Yo can search for movies by titles, director, actor,
+        genre, or release date, makeing it easy to find the perfect movie for
+        any occasion.
+      </p>
+      <p>
+        In addition to our extensive movie database, we also offer a platform
+        for movies lovers to connect and share their thoughts on the latest
+        releases. Our comunity section includes a forum where you can discuss
+        you favorite flims with like-minded individuals and read reviews and
+        ratings from other users. We also have a section dedicated to movie news
+        and trailers, keeping you updated to the latest happening in the world
+        of cinema. Thank you for waiting our website and w hope you enjoy your
+        time browsing through our movie database. If you have nay feedback or
+        suggestions, please feel free to contact us. We are looking for way to
+        improve and enhance the user experience on our website.
+      </p>
+    </div>
+  );
+}
